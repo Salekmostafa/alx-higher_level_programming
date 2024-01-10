b3836a25c8f195f4534cd329e29c24d97a131b98
Salekmostafa <salekmostafa@gmail.com>
2024-01-10 02:07:19 +0100

    for

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_str = ""
+    for char in my_string:
+        if char.lower() not in ['c', 'C']:
+            new_str += char
+    return new_string
