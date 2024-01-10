65acc2e9891bba3a812573b78cfd90cfd71fd200
Salekmostafa <salekmostafa@gmail.com>
2024-01-10 23:28:12 +0100

    for import

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
+
 if __name__ == "__main__":
-    from hidden_4 import *
-    for str in dir():
-        if str[:2] != "__":
-            print("{}".format(str))
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
