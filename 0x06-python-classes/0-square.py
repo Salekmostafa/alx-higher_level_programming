4c3867c326b2da87e6c1bac043cca3e41e6c6cc0
Salekmostafa <salekmostafa@gmail.com>
2024-01-24 03:39:18 +0100

    for py class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A simple implementation of a Square class"""
+
+
+
+class Square:
+    """This class defines a basic square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class named Square that represents a square"""
+
+
+class Square:
+    """Class Square: represents a square"""
+    def __init__(self, size):
+        """Initialize the square.
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
